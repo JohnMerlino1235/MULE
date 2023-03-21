50c4ffcaf42d96cabb4ecf0e5fd1f92616d985c3
JohnMerlino1235 <john.merlino1235@gmail.com>
2023-03-21 17:15:22 -0400

    Update endpoints

--- endpoints/endpoints.py
+++ endpoints/endpoints.py
@@ -1,41 +1,11 @@
-from db.model.database import Database
-from db.model.database_user import DatabaseUserModel
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
-@app.route('/fetch_user_from_db/<email>', methods=['POST'])
-def fetch_user_from_db(email):
-    # temporary_user_model = DatabaseUserModel(email, 'somePassword', 'someFirstName', 'someLastName')
-    print("User fetched from database with email ", email)
-    response = jsonify({'email': email})
-    return response
-    # return temporary_user_model.fetch_user_from_database()
-
-@app.route('/upsert_user_to_db')
-def upsert_user_to_db(email, password, first_name, last_name):
-    # temporary_user_model = DatabaseUserModel(email, password, first_name, last_name)
-    # temporary_user_model.upsert_user_to_database()
-    print("User upserted to database with email")
-    return
-
-@app.route('/register_user/<email>', methods=['POST'])
-def register_user(email, password, first_name, last_name):
-    user_database_model = DatabaseUserModel(email, password, first_name, last_name)
-    fetched_user_from_db = user_database_model.fetch_user_from_database()
-    # First we fetch the user from our database to check to see if they are already registered
-    if fetched_user_from_db:
-        print("User already registered in our database")
-        return
-    user_database_model.upsert_user_to_database()
-
-    print("User successfully registered")
-    return
-
 @app.route('/login_user/<email>', methods=['POST'])
 def login_user(email, password):
+    from db.model.database_user import DatabaseUserModel
     # to determine if a user has a valid account, we must fetch their username from the database.
     user_database_model = DatabaseUserModel(email, password)
     fetched_user_from_db = user_database_model.fetch_user_from_database()
@@ -57,6 +27,7 @@ def login_user(email, password):
 
 @app.route('/sign_up', methods=['POST'])
 def sign_up(email, password, first_name, last_name):
+    from db.model.database import Database
     database_connection = Database()
 
     # to ensure this user does not already have an account, we can do a quick db fetch based on the input email
