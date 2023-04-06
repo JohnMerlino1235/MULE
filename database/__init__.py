ccba1719cc70f88c32c74ef40c78cf48624c18a9
JohnMerlino1235 <john.merlino1235@gmail.com>
2023-04-06 15:39:13 -0400

    Db fix

--- database/__init__.py
+++ database/__init__.py
@@ -1,2 +1,2 @@
 from database.user import User, db
-from database.data import Data, db
+from database.data import Data
--- database/data.py
+++ database/data.py
@@ -1,8 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
-db = SQLAlchemy()
-
+from database import db
 class Data(db.Model):
     __tablename__ = 'data'
 
